6fef76c1a6e7e1662d978a3e5bc8ef1a3838e42b
Rdrg974 <hassany.rodrigue@gmail.com>
2023-12-14 01:58:14 -0800

    Handle command lines with arguments

---
+++ argument.c
@@ -0,0 +1,27 @@
+#include "simple_shell.h"
+
+/**
+ * get_argument - handle command lines with arguments
+ * @buffer: path + argument
+ * Return: an array of each element of path
+ */
+
+char **get_argument(char *buffer)
+{
+	int i = 0;
+	char *tmp = malloc(strlen(buffer) + 1);
+	char *token;
+	char **args = malloc(sizeof(char *) * 10);
+
+	strcpy(tmp, buffer);
+	token = strtok(tmp, " ");
+	while (token)
+	{
+		args[i] = malloc(sizeof(char *) + 5);
+		strcpy(args[i], token);
+		token = strtok(NULL, " ");
+		i++;
+	}
+	args[i] = NULL;
+	return (args);
+}

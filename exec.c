4a6adadf3c56a757d9ac740b95c6fc04ce9c01ca
Rdrg974 <hassany.rodrigue@gmail.com>
2023-12-20 03:38:01 -0800

    Adding the function free args

--- exec.c
+++ exec.c
@@ -1,6 +1,19 @@
 #include "simple_shell.h"
 
 /**
+ * free_args - free each element of array args
+ * @args: an array of command
+ */
+void free_args(char **args)
+{
+	int i;
+
+	for (i = 0; args[i] != NULL; i++)
+		free(args[i]);
+	free(args);
+}
+
+/**
  * execute_command - allows a process to execute another program
  * @buffer: a string of command shell
  * @name_programm: the name of progammi
@@ -18,10 +31,14 @@ void execute_command(char *buffer, char *name_programm, int nbr_command)
 	{
 		args = get_argument(buffer);
 		command_path = get_path(args[0]);
+		if (command_path == NULL)
+		{
+			fprintf(stderr, "%s: %d: %s: not found\n",
+					name_programm, nbr_command, args[0]);
+			free_args(args), exit(EXIT_FAILURE);
+		}
 		execve(command_path, args, environ);
-		fprintf(stderr, "%s: %d: %s: not found\n",
-				name_programm, nbr_command, args[0]);
-		exit(EXIT_FAILURE);
+		perror("execve"), free_args(args), exit(EXIT_FAILURE);
 	}
 	else
 		wait(0);
